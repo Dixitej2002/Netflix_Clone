98fe7b687019133d69557e22437fccc776187433
I AM Dixit <sik.amitkumar@gmail.com>
2024-10-08 11:53:10 +0530

    "Updated subproject commit reference in netflixclone"

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
         <div className='absolute z-10 flex w-[100%] px-6 items-center justify-between bg-gradient-to-b from-black '>
             <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="Netflix logo" />
             <div className='flex items-center'>
-                <IoIosArrowDropdown size={24}/>
-                <h1 className='text-lg font-medium'>Dixit Pradhan</h1>
+                <IoIosArrowDropdown size={24} color='white'/>
+                <h1 className='text-lg font-medium text-white'>Dixit Pradhan</h1>
                 <div className='ml-5'>
 
                     <button className='bg-red-800 text-white px-4 py-2'>Logout</button>
--- src/component/Login.js
+++ src/component/Login.js
@@ -7,9 +7,21 @@ const Login = () => {
 
     <div>
         <Header/>
-        <div>
+        <div className='absolute'>
             <img className='w-[100vw] h-[100vh]' src="https://wallpapers.com/images/high/netflix-background-gs7hjuwvv2g0e9fj.webp" alt="Banner" />
         </div>
+
+        <form action="" className='flex p-12 flex-col w-3/12 my-36 left-0 right-0 mx-auto items-center justify-center absolute bg-black opacity-85'>
+        {/* <form action="" className='absolute bg-black items-center justify-center  top-40 flex-col content-center'> */}
+        <h1 className='text-white font-bold mb-5 text-3xl '>Signup</h1>
+          <div className='flex flex-col'>
+          
+            <input type="text" placeholder='Full Name' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'/>
+            <input type="email" placeholder='Email' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white' />
+            <input type="password" placeholder='Password' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'/>
+           <p>Already have an account?</p>
+          </div>
+        </form>
     </div>
 
     </>
